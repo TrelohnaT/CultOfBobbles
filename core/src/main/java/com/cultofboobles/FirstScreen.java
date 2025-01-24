d8e909fb9ffb41c9bf7266ef2e7e1f790e0b5b6f
TrelohnaT <48327399+TrelohnaT@users.noreply.github.com>
2025-01-24 21:29:45 +0100

    added hitbox of viewport

--- core/src/main/java/com/cultofboobles/FirstScreen.java
+++ core/src/main/java/com/cultofboobles/FirstScreen.java
@@ -3,19 +3,33 @@ package com.cultofboobles;
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.cultofboobles.entity.Entity;
+import com.cultofboobles.entity.Player;
 import com.cultofboobles.ui.UiHandler;
+import com.cultofboobles.utils.HitBox;
 import com.cultofboobles.view.ViewStuffHandler;
 
-/** First screen of the application. Displayed after the application is created. */
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * First screen of the application. Displayed after the application is created.
+ */
 public class FirstScreen implements Screen {
 
     private final Game agame;
 
     private SpriteBatch spriteBatch;
+    private ShapeRenderer sr;
+
+    private final Map<String, Entity> entityMap = new HashMap<>();
+
 
     private ViewStuffHandler viewStuffHandler;
     private UiHandler uiHandler;
@@ -29,14 +43,18 @@ public class FirstScreen implements Screen {
     public void show() {
         // Prepare your screen here.
         spriteBatch = new SpriteBatch();
+        sr = new ShapeRenderer();
+
         viewStuffHandler = new ViewStuffHandler();
         uiHandler = new UiHandler();
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
         stage.addActor(uiHandler.getUi());
-        stage.setDebugAll(true);
+        // to see borders of UI elements
+        //stage.setDebugAll(true);
 
+        prepareEntites();
 
     }
 
@@ -45,9 +63,20 @@ public class FirstScreen implements Screen {
         // Draw your screen here. "delta" is the time since last render in seconds.
         Main.timeElapsed += delta;
 
+        Entity player = entityMap.get("player");
+
         handleViewPort();
 
         drawUi();
+
+        entityMap.forEach((id, entity) -> entity.update());
+
+
+        if(!viewStuffHandler.getActualViewPort().contains(player.getHitBox())) {
+            player.hitObstacle();
+        }
+
+        drawHitBoxes();
 
     }
 
@@ -79,6 +108,19 @@ public class FirstScreen implements Screen {
         // Destroy screen's assets here.
     }
 
+    private void prepareEntites() {
+
+        this.entityMap.put(
+            "player",
+            new Player(
+                "player",
+                "",
+                Gdx.graphics.getWidth()/2,
+                Gdx.graphics.getHeight()/2
+            )
+        );
+
+    }
 
     private void handleViewPort() {
         // Draw your screen here. "delta" is the time since last render in seconds.
@@ -93,6 +135,31 @@ public class FirstScreen implements Screen {
     private void drawUi() {
         stage.act(Gdx.graphics.getDeltaTime());
         stage.draw();
+    }
+
+    private void drawHitBoxes() {
+        sr.setProjectionMatrix(viewStuffHandler.getViewport().getCamera().combined);
+        sr.begin(ShapeRenderer.ShapeType.Line);
+        for (var entry : entityMap.entrySet()) {
+            Rectangle tmp = entry.getValue().getHitBox();
+            sr.setColor(new Color(0, 0, 1, 0));
+            sr.rect(tmp.x, tmp.y, tmp.width, tmp.height);
+        }
+
+        sr.setColor(new Color(0, 0, 0, 0));
+        sr.rect(
+            viewStuffHandler.getActualViewPort().x,
+            viewStuffHandler.getActualViewPort().y,
+            viewStuffHandler.getActualViewPort().width,
+            viewStuffHandler.getActualViewPort().height
+        );
+
+        sr.end();
+
+    }
+
+    private void checkCollisions() {
+
     }
 
 }
